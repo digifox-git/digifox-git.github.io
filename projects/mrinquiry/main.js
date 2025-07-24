79f86e569e12796ab00757b3738bd88908c000a9
digifox <digifox08@gmail.com>
2025-07-23 22:52:27 -0500

    secret stuffs and search

--- projects/mrinquiry/main.js
+++ projects/mrinquiry/main.js
@@ -1,48 +1,55 @@
-var successAudio = new Audio('websuccess.mp3')
+var successAudio = new Audio('https://digifox.space/projects/mrinquiry/websuccess.mp3')
 successAudio.load()
-var errorAudio = new Audio('webnotice.mp3')
+successAudio.volume=0.5
+var errorAudio = new Audio('https://digifox.space/projects/mrinquiry/webnotice.mp3')
 errorAudio.load()
-var publishAudio = new Audio('webpublish.mp3')
+errorAudio.volume=0.5
+var publishAudio = new Audio('https://digifox.space/projects/mrinquiry/webpublish.mp3')
 publishAudio.load()
+publishAudio.volume=0.5
 
-function success() {
-    successAudio.load()
-    successAudio.play()
-}
-function error() {
-    errorAudio.load()
-    errorAudio.play()
-}
-function publish() {
-    publishAudio.load()
-    publishAudio.play()
-}
 
-function show_message(type, header, message) {
+function show_message(type, header, message, image) {
     var element = document.getElementById('notification');
     var elementcontainer = document.getElementById('notificationcontainer');
     var elementheader = document.getElementById('notifheader')
     var elementmessage = document.getElementById('notifmessage')
     var root = document.querySelector(':root')
+    var rand = Math.floor(Math.random() * (10000 - 1 + 1)) + 1
+    console.log(`Generated a random value! Yours was ${rand} out of 10000!.`)
 
-
+    if (rand == 66) {
+        window.location.replace('tab/w.html')
+    }
 
     if (type == 'success') {
-        successAudio.load()
+        successAudio.currentTime = 0;
         successAudio.play()
-        root.style.setProperty('--hue', '280deg')
+        root.style.setProperty('--hue', '300deg')
     }
 
     if (type == 'error') {
-        errorAudio.load()
+        errorAudio.currentTime = 0;
         errorAudio.play()
         root.style.setProperty('--hue', '160deg')
     }
 
     if (type == 'publish') {
-        publishAudio.load()
+        publishAudio.currentTime = 0;
         publishAudio.play()
         root.style.setProperty('--hue', '20deg')
+    }
+
+    if (type == 'creepy') {
+        publishAudio.currentTime = 0;
+        publishAudio.play()
+        root.style.setProperty('--hue', '150deg')
+    }
+
+    if (type == 'basic') {
+        publishAudio.currentTime = 0;
+        successAudio.play()
+        root.style.setProperty('--hue', '0deg')
     }
 
     if(element.style.display == 'none' ||element.style.display == '') {
@@ -50,6 +57,14 @@ function show_message(type, header, message) {
         elementcontainer.style.display = 'flex';
         elementheader.innerHTML = header
         elementmessage.innerHTML = message
+        if (image != '' || image != null) {
+            let imgDiv = document.getElementById('optionalimg')
+            let notifImage = document.createElement("img")
+            notifImage.src = image
+            notifImage.id = "notifimage"
+            imgDiv.appendChild(notifImage)
+
+        }
     }
 
 }
@@ -57,20 +72,36 @@ function show_message(type, header, message) {
 function hide_message() {
     var element = document.getElementById('notification');
     var elementcontainer = document.getElementById('notificationcontainer');
+    let hasImage = document.getElementById("notifimage")
 
     if(element.style.display == 'flex') {
         element.style.display = 'none';
         elementcontainer.style.display = 'none';
+        hasImage.remove();
     }
 }
 
+function download_standalone() {
+    window.location.href = 'https://digifox.space/Resources/DOWNLOAD/TROPIC.zip'
+}
+
+function request(url) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, true); // false for synchronos request
+    xmlHttp.send(null)
+    console.log(xmlHttp.responseText)
+    return xmlHttp.responseText
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-    let snd_move = new Audio("webhover.mp3");
+    let snd_move = new Audio("https://digifox.space/projects/mrinquiry/webhover.mp3");
     snd_move.volume = 0.5;
+    snd_move.load()
 
     async function movesnd() {
-    snd_move.load();
+    snd_move.currentTime = 0
     snd_move.play();
+    console.log('played')
     }
 
     document.querySelectorAll("button").forEach(button => {
---
+++ projects/woke-o-meter/main.js
@@ -0,0 +1,5 @@
+function update() {
+    let value = document.getElementById('value').value
+    let juice = document.getElementById('juice')
+    console.log(value)
+}
