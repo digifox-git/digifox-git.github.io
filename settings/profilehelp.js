0d46dc395fc6a0c49112a78bc2f46e7bacc486a2
digifox <digifox08@gmail.com>
2025-08-10 18:06:26 -0500

    gallery button

--- settings/profilehelp.js
+++ settings/profilehelp.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let helpButton = document.getElementById("profilehelp")
 
     helpButton.addEventListener("click", function() {
-        show_help('All about... you!?', '&nbsp&nbsp This is the settings page! Every interactive elements of the site are stored here. Some of them (such as you name and profile image) can be changed fairly easily. Others (such as your inventory and Fun Value) cannot be changed as easily. Resetting your profile will delete every value related to this site in your web browsers local storage.<br><br>Oh, and one more thing: <u>Everything listed on this page is stored locally. It isnt saved anywhere online, and I dont plan to do that ever!!</u>')
+        show_help('All about... you!?', '&nbsp&nbsp This is your profile! Every interactive elements of the site are stored here. Some of them (such as you name and profile image) can be changed fairly easily. Others (such as your inventory and Fun Value) cannot be changed as easily. Resetting your profile will delete every value related to this site in your web browsers local storage.<br><br>Oh, and one more thing: <u>Everything listed on this page is stored locally. It isnt saved anywhere online, and I dont plan to do that ever!!</u>')
         console.log("success")
     })
 
