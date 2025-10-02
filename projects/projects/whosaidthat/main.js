60e15292a769e40a9e3d464be2a9e8115c7f6a78
digifox <digifox08@gmail.com>
2025-10-02 16:02:34 -0500

    a

---
+++ projects/projects/whosaidthat/main.js
@@ -0,0 +1,567 @@
+let questions = {
+    "0": {
+        "content": "How to usurp the trials which assail you Google ai",
+        "answer": "Jon Mark"
+    },
+    "1": {
+        "content": "blink twice if you're FEELING EARNEST!!!",
+        "answer": "Skylar"
+    },
+    "2": {
+        "content": "yeah i actually feel really bad im sorry link <br>im still mad at you though so dont talk in Discord for 3 days",
+        "answer": "Matthew"
+    },
+    "3": {
+        "content": "i feel like Pim in that one smiling friends episode where he eats worms",
+        "answer": "Matthew"
+    },
+    "4": {
+        "content": "If this message gets 14 likes, Link gets drafted into the military ⬇️⬇️⬇️⬇️⬇️⬇️",
+        "answer": "Jon Mark"
+    },
+    "5": {
+        "content": "HEY GUYS IS ME I LOVCE MEN",
+        "answer": "Jon Mark"
+    },
+    "6": {
+        "content": "If the early bird gets the worm, I’ll sleep in until there’s pancakes.",
+        "answer": "Julia"
+    },
+    "7": {
+        "content": "'hello mr president' <br>worm controlled president: 'more dirt'",
+        "answer": "Link"
+    },
+    "8": {
+        "content": "gonna turn you into gravy tonight",
+        "answer": "Carl"
+    },
+    "9": {
+        "content": "splig donuts",
+        "answer": "Link"
+    },
+    "11": {
+        "content": "Today's Podcast Title: Meagan Thee Bubble",
+        "attachment": "assets/image_webfish.png",
+        "answer": "Carl"
+    },
+    "12": {
+        "content": "this",
+        "attachment": "assets/monitor.png",
+        "answer": "Carl"
+    },
+    "13": {
+        "content": "I think that there's a skinwalker in my home, I swear I heard my sister's voice but she isn't home",
+        "answer": "Ryan"
+    },
+    "14": {
+        "content": "penis adventures",
+        "answer": "Skylar"
+    },
+    "15": {
+        "content": "All Inquiries is a vacuum of what link thinks we would all act like online that he projects",
+        "answer": "Jon Mark"
+    },
+    "16": {
+        "content": "I bring a real “take me to your leader 👽😛” vibe that the rural Wisconsinite driving a pick up truck on a farmer’s road did not like",
+        "answer": "Jon Mark"
+    },
+    "17": {
+        "content": "Gonna make a drink with 13 types of root beers mixed and call It Jon's Jungle Juice",
+        "answer": "David"
+    },
+    "18": {
+        "content": "Home is where you hang your antlers 🦌❤️",
+        "answer": "Josh"
+    },
+    "19": {
+        "content": "my brother is practicing his oboe and it sounds like he's being told off by a dont starve character",
+        "answer": "Skylar"
+    },
+    "20": {
+        "content": "When the imposter is bodacious!",
+        "answer": "Link"
+    },
+    "21": {
+        "content": "sure!<br>if you catch me first! *scurries away into the cracks in the ceiling like a little bug*",
+        "answer": "Link"
+    },
+    "22": {
+        "content": "send the rot<br>🤷the rot consumes",
+        "answer": "Link"
+    },
+    "23": {
+        "content": "STOP MESSING THINGS UP OR I WILL KILL YOU",
+        "answer": "Logan"
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "16": {
+        "content": "",
+        "answer": ""
+    },
+    "90": {
+        "content": "",
+        "answer": ""
+    },
+    "91": {
+        "content": "",
+        "answer": ""
+    },
+    "92": {
+        "content": "",
+        "answer": ""
+    },
+    "93": {
+        "content": "",
+        "answer": ""
+    },
+    "94": {
+        "content": "",
+        "answer": ""
+    },
+    "95": {
+        "content": "",
+        "answer": ""
+    },
+    "96": {
+        "content": "",
+        "answer": ""
+    },
+    "97": {
+        "content": "",
+        "answer": ""
+    },
+    "98": {
+        "content": "",
+        "answer": ""
+    },
+    "99": {
+        "content": "",
+        "answer": ""
+    },
+    "100": {
+        "content": "",
+        "answer": ""
+    },
+}
+
+let possibleNames = [
+    "Link",
+    "Carl",
+    "Skylar",
+    "Matthew",
+    "Ryan",
+    "Aubrey",
+    "Jon Mark",
+    "Bree",
+    "Josh",
+    "Julia",
+    "Trey",
+    "Christian",
+    "Logan",
+    "David",
+    "Oli",
+    "Troy",
+    "Bret",
+    "Carly",
+]
+
+let correctAnswer = ""
+let scoreGain = 0
+let totalScore = 0
+let health = 3
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    generate_question()
+
+})
+
+function generate_question() {
+
+    let questionContent = document.getElementById("question-content")
+    let questionText = document.getElementById("question-text")
+    let answerChoice = document.querySelectorAll(".option")
+
+    // If the previous question had an image, remove it
+    if (document.getElementById("question-image")) {
+        document.getElementById("question-image").remove()
+    }
+
+    const questionKeys = Object.keys(questions)
+    const randKey = questionKeys[Math.floor(Math.random() * questionKeys.length)]
+
+    let tempNameList = possibleNames.slice() // Create temporary name list that entries can be removed from to prevent duplicates
+
+    // Choose a random name for each of the four answer choices
+    answerChoice.forEach(element => {
+
+        const randName = Math.floor(Math.random() * tempNameList.length)
+
+        element.innerHTML = tempNameList[randName]
+        tempNameList.splice(randName, 1) // Remove the selected name from the tempNameList array
+
+    });
+
+    // Pick a random answer choice and replace the random name with the correct answer
+    const randChoice = Math.floor(Math.random() * answerChoice.length)
+    answerChoice[randChoice].innerHTML = questions[randKey].answer
+
+    // Show the user message and play an animation
+    questionText.classList.remove("zoomin");
+    void questionText.offsetWidth;
+    questionText.classList.add("zoomin");
+    questionText.innerHTML = `"${questions[randKey].content}"`
+
+    // If neccessary, create an image and play an animation
+    if (questions[randKey].attachment) {
+        let questionImage = document.createElement('img')
+        questionImage.id = "question-image"
+        questionImage.src = questions[randKey].attachment
+        questionContent.appendChild(questionImage)
+        questionImage.classList.remove("zoomout");
+        void questionImage.offsetWidth;
+        questionImage.classList.add("zoomout");
+    }
+
+    // Set score gain
+    if (questions[randKey].content.length < 10) {
+        scoreGain = 100
+    } else if (questions[randKey].content.length < 15) {
+        scoreGain = 95
+    } else if (questions[randKey].content.length < 20) {
+        scoreGain = 90
+    } else if (questions[randKey].content.length < 25) {
+        scoreGain = 85
+    } else if (questions[randKey].content.length < 30) {
+        scoreGain = 80
+    } else if (questions[randKey].content.length < 35) {
+        scoreGain = 75
+    } else if (questions[randKey].content.length < 40) {
+        scoreGain = 70
+    } else if (questions[randKey].content.length < 45) {
+        scoreGain = 65
+    } else {
+        scoreGain = 60
+    }
+
+    correctAnswer = questions[randKey].answer
+}
+
+function submit_answer(clickedElement) {
+
+    let score = document.getElementById("score")
+    let status = document.getElementById("answer-popup")
+
+    if (clickedElement.innerHTML == correctAnswer) {
+        console.log("Correct!")
+
+        // Add and show updated score
+        totalScore += scoreGain
+        score.innerHTML = `${totalScore} pts`
+
+        // Show correct status animation
+        status.innerHTML = "Correct!"
+        status.classList.remove("flashgreen");
+        status.classList.remove("flashred");
+        void status.offsetWidth;
+        status.classList.add("flashgreen");
+
+    } else {
+        console.log("Incorrect!")
+
+        // Decrement Health
+        update_health(-1)
+
+        // Show incorrect status animation
+        status.innerHTML = "Incorrect!"
+        status.classList.remove("flashgreen");
+        status.classList.remove("flashred");
+        void status.offsetWidth;
+        status.classList.add("flashred");
+    }
+
+    generate_question()
+}
+
+function update_health(number) {
+
+    let healthDisplay = document.getElementById("lifecount")
+
+    health += number
+
+    console.log(health)
+
+    healthDisplay.innerHTML = ""
+
+    for(let i = 0; i < health; i++) {
+        healthDisplay.innerHTML += "♥"
+    }
+
+    if (health == 0) {
+        game_over()
+    }
+
+}
+
+function game_over() {
+    window.location.href ="index.html"
+}
