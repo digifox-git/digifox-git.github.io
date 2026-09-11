38ec4ea882d630a22c647a2797c03d5d78fced9f
digifox <digifox08@gmail.com>
2026-09-11 15:31:54 -0500

    Styling

---
+++ Projects/projects/schedulematch/main.js
@@ -0,0 +1,24 @@
+let schedules = {
+    "Matthew": {
+        "Precal A": {
+            "start": {
+                "hour": 10,
+                "minute": 0
+            },
+            "end": {
+                "hour": 10,
+                "minute": 50
+            }
+        },
+        "Rhetoric & Composition": {
+            "start": {
+                "hour": 11,
+                "minute": 0
+            },
+            "end": {
+                "hour": 11,
+                "minute": 50
+            }
+        }
+    }
+}
